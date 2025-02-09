697bd1c3ad30fb72cc20b326664f03957d2092e7
Texnolog24 <latkina0808@yandex.ru>
2025-02-09 23:45:04 +0500

    Внесли изменения в проект

---
+++ src/test/java/ProductManager/ShopRepositoryTest.java
@@ -0,0 +1,43 @@
+package ProductManager;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ShopRepositoryTest {
+
+    @Test
+    public void testRemoveWhenProductExist() {
+        ShopRepository repo = new ShopRepository();
+
+        Product product1 = new Product(1, "телефон", 30000);
+        Product product2 = new Product(2, "книга", 300);
+        Product product3 = new Product(3, "машина", 3000000);
+
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+        repo.remove(2);
+        Product[] actual = repo.findAll();
+        Product[] expected = {product1, product3};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void testRemoveWhenProductNotFoundExist() {
+        ShopRepository repo = new ShopRepository();
+
+        Product product1 = new Product(1, "телефон", 30000);
+        Product product2 = new Product(2, "книга", 300);
+        Product product3 = new Product(3, "машина", 3000000);
+
+        repo.add(product1);
+        repo.add(product2);
+        repo.add(product3);
+
+        Assertions.assertThrows(NotFoundException.class, () -> repo.remove(2456));
+    }
+}
